d212649bc474b968bb6c4053dbccb51174fef422
Tarek Kalaji <tarek.it.eng@gmail.com>
2024-02-23 23:07:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='bigbluebutton_api_python',
       version='0.0.12',
       description='Python library that provides access to the API of BigBlueButton',
-      author='Tarek Kalaji, Yunkai Wang',
+      author='Tarek Kalaji, Yunkai Wang, Hicham Dachir',
       author_email='yunkai.wang@blindsidenetworks.com',
       url='https://github.com/yunkaiwang/bigbluebutton-api-python',
       packages=find_packages(),
